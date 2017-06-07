30ef89ac6b2a4067973f22895b56e01593578c6f
al-little <alisdair.little@gmail.com>
2017-06-07 09:04:42 +0100

    Read configuration settings

--- Controller.py
+++ Controller.py
@@ -1,4 +1,5 @@
-import pyrebase, configparser
+import pyrebase
+import configparser
 
 class Controller:
     config = {
@@ -8,45 +9,59 @@ class Controller:
         "storageBucket": "rocket-telemetry.appspot.com"
     }
 
+    running = True  # Main loop
+    user = None  # Required to execute firebase commands
+    firebase = None
+    db = None
+    sampleInterval = 250  # Default sample interval
+    sendInterval = 5000  # Default send interval
+
     def __init__(self):
-        "Read telemetry settings"
-        print('init')
+        print('Init Controller')
         self.firebase = pyrebase.initialize_app(self.config)
+        self.readconfig()
 
-        self.readConfig()
-
-    def readConfig(self):
-        print('readConfig')
+    def readconfig(self):
         auth = self.firebase.auth()
-
         config = configparser.ConfigParser()
-        config.read('defaults.cfg')
 
+        "Read the firebase configuration information"
+        config.read('defaults.cfg')
         username = config.get('auth', 'username')
         password = config.get('auth', 'password')
 
+        "Sign into firebase, to allow reads and writes"
         self.user = auth.sign_in_with_email_and_password(username, password)
+        self.db = self.firebase.database()
 
-        db = self.firebase.database()
+        "Read the device information"
+        result = self.db.child('device_info').get(self.user['idToken'])
+        results = result.val()
 
-        result = db.child('device_info').get(self.user['idToken'])
+        print(results)
 
-        print(result.val())
+        self.sendInterval = results['send_interval']
+        self.sampleInterval = results['sample_interval']
+
+    def readcommand(self):
+        return False
 
     def run(self):
+        print('Running...')
+        sampleCount = 0
         "Get sensors"
 
         "Find settings"
 
-        "Main loop"
-        "   Process Commands - Need a way to simulate (command line?)"
-        "       Action (start camera, start gps etc"
-        "           If gps is started then calling get readings will give you data"
-        "       Log commands"
-        "       Update device mode if necessary"
-        "           Log device mode change"
-        "   "
-        "   Get readings"
-        "       Log to disk"
-        "       If enough samples"
-        "           db.child('telemetry/telemetry_0X/data').put"
+        while self.running:
+            if self.readcommand():
+                print('Processing command...')
+                "Update firebase if the device mode changes"
+
+            print("Collecting samples..." + str(sampleCount))
+
+            sampleCount = sampleCount + self.sampleInterval
+
+            if sampleCount >= self.sendInterval:
+                print('[Send samples]')
+                sampleCount = 0
