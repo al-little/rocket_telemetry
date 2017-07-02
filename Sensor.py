a395f19cdbbd889923be6481475bc70dd61f70bf
Al Little <alisdair.little@gmail.com>
2017-07-02 10:49:02 +0100

    Delete Sensor.py
    Refactoring

--- Sensor.py
+++
@@ -1,9 +0,0 @@
-''' The Sensor class can control a physical sensor.
-    When in test mode it can give you realistic data.
-'''
-class Sensor:
-    def __init__(self, thing):
-        self.name = thing
-
-    def getReading(self):
-        return None
