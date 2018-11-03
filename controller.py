b2d35b30092d82052752a262092bbdb9d57224dd
Alisdair Little <al@thinkculture.net>
2018-11-03 11:07:36 +0000

    New config items to handle if hardware is present

--- controller.py
+++ controller.py
@@ -17,8 +17,10 @@ class Controller(object):
     log_path = ''
     image_path = ''
     video_path = ''
-    camera = Camera('PI Camera')
+    camera_present = False
+    enviro_present = False
     global log_file
+    global camera
 
     def __init__(self):
         'Initialise the firebase instance and read the config'
@@ -28,78 +30,38 @@ class Controller(object):
         path = self.log_path + 'log_' + str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")) + '.csv'
 
         self.log_file = open(path, "a")
+        self.log_file.write("Starting telemetry {0}\n".format(datetime.datetime.utcnow()))
 
         signal.signal(signal.SIGTERM, self.stop)
         signal.signal(signal.SIGHUP, self.ignore)
 
+    '''Detect system terminate'''
     def stop(self, sig, frame):
         print('System shutdown')
         self.camera.stop_video()
+        self.log_file.write("System shutdown {0}\n".format(datetime.datetime.utcnow()))
         log_file.close()
 
+    '''Detect system hangup'''
     def ignore(self, sig, frame):
         print('ignoring signal %d\n' % sig)
+        self.log_file.write("Ignore signal {0}\n".format(datetime.datetime.utcnow()))
 
-    """readConfig looks at the defaults.cfg file.
-    If a Fona board is present then initialise it and detect if we have 3G
-    GPS?
-    """
+    '''readConfig looks at the defaults.cfg file.'''
     def readconfig(self):
-        'Read the configuration information from firebase'
         config = configparser.ConfigParser()
-
-        # Read the firebase configuration information
         config.read('defaults.cfg')
+
         self.image_path = config.get('paths', 'image_path')
         self.video_path = config.get('paths', 'video_path')
         self.log_path = config.get('paths', 'log_path')
 
-    def readcommand(self):
-        while len(self.commands) > 0:
-            "Update firebase if the device mode changes"
-            "Play noise/Update lights to reflect state"
-            "Start new session"
-            "   Create a new directory ready for data"
-            "Stop current session"
-            command = self.commands.pop()
+        self.camera_present = config.get('devices', 'camera')
+        self.enviro_present = config.get('devices', 'enviro')
 
-            print('Processing command: [' + command + ']')
-
-            if len(command) > 0:
-                object = command[0].upper()
-
-                if object == 'C':
-                    if len(command) > 1:
-                        try:
-                            cmd = CameraCommand(int(command[1]))
-
-                            if cmd == CameraCommand.STILL_PHOTO:
-                                self.camera.take_picture(self.image_path)
-                            elif cmd == CameraCommand.START_VIDEO:
-                                self.camera.start_video(self.video_path)
-                            elif cmd == CameraCommand.STOP_VIDEO:
-                                self.camera.stop_video()
-                            break
-                        except ValueError:
-                            print('Unrecognised Camera Command')
-                elif object == 'S':
-                    print('Speaker')
-                elif object == 'E':
-                    print('Enviro')
-                elif object == 'A':
-                    print('All')
-                    if len(command) > 1:
-                        cmd = command[1]
-
-                        if cmd == '1':
-                            print('All on')
-                            "Collect samples, start video"
-                        else:
-                            print('All off')
-
-                "Either acknowledge or update firebase"
-
-                "Process: C - Camera, S - Speaker, E - Enviro, A - All"
+        if self.camera_present:
+            self.log_file.write("Starting camera {0}\n".format(datetime.datetime.utcnow()))
+            self.camera = Camera('PI Camera')
 
     '''
     Design:
@@ -110,8 +72,17 @@ class Controller(object):
         'Main run loop that processes commands and records sensor data'
         print('Running...')
 
+        self.log_file.write("Starting main loop {0}\n".format(datetime.datetime.utcnow()))
+
+        if self.camera_present:
+            self.camera.start_video(self.video_path)
+
         'initial altitude'
-        sea_level = weather.altitude()
+        if self.enviro_present:
+            sea_level = weather.altitude()
+        else:
+            sea_level = 0
+
         last_altitude = 0
         start_clock = False
         parachute_deployed = False
@@ -119,45 +90,46 @@ class Controller(object):
         timer = 0
 
         while self.running:
-            self.commands.append(input("Command >"))
-            self.readcommand()
+            if self.enviro_present:
+                altitude = weather.altitude()
+                acc_values = [round(x, 2) for x in motion.accelerometer()]
+                accel_x = acc_values[0]
+                accel_y = acc_values[1]
+                accel_z = acc_values[2]
 
-            altitude = weather.altitude()
-            acc_values = [round(x, 2) for x in motion.accelerometer()]
-            accel_x = acc_values[0]
-            accel_y = acc_values[1]
-            accel_z = acc_values[2]
+                try:
+                    self.log_file.write("{0},{1},{2},{3},{4}\n".format(datetime.datetime.utcnow(),
+                                                                       str(altitude - sea_level),
+                                                                       str(accel_x),
+                                                                       str(accel_y),
+                                                                       str(accel_z)))
+                    self.log_file.flush()
+                except:
+                    print('logging failed')
 
-            try:
-                self.log_file.write("{0},{1},{2},{3},{4}\n".format(datetime.datetime.utcnow(),
-                                                                   str(altitude - sea_level),
-                                                                   str(accel_x),
-                                                                   str(accel_y),
-                                                                   str(accel_z)))
-            except:
-                print('logging failed')
+                if altitude - sea_level > 10:
+                    start_clock = True
 
-            if altitude - sea_level > 10:
-                start_clock = True
+                # If the height is greater than 10m then start running this check.
+                if not hit_apogee and last_altitude < altitude and start_clock:
+                    hit_apogee = True
+                    print('Apogee')
 
-            # If the height is greater than 10m then start running this check.
-            if not hit_apogee and last_altitude < altitude and start_clock:
-                hit_apogee = True
-                print('Apogee')
+                if altitude <= 90 and not parachute_deployed:
+                    parachute_deployed = True
+                    print('Deploy parachute')
 
-            if altitude <= 90 and not parachute_deployed:
-                parachute_deployed = True
-                print('Deploy parachute')
+                last_altitude = altitude
 
-            last_altitude = altitude
             time.sleep(self.sampleInterval)
-
-            self.log_file.flush()
 
             if start_clock:
                 timer += self.sampleInterval
 
                 if timer > self.cutoffTime:
-                    self.camera.stop_video()
-                    log_file.close()
+                    if self.camera_present:
+                        self.log_file.write("Auto shutdown... {0}\n".format(datetime.datetime.utcnow()))
+                        self.camera.stop_video()
+
+                    self.log_file.close()
                     self.running = False
